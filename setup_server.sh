d9ee9b7c57a6b935528ea14ccd0569f9a67e8d90
Robert Kimutai <robertkimutai7@gmail.com>
2021-06-08 20:16:43 +0300

    Add some color to shell scripts

--- setup_server.sh
+++ setup_server.sh
@@ -1,31 +1,31 @@
-echo "INSTALL DOCKER ENGINE"
+tput setaf 4; echo "INSTALL DOCKER ENGINE"
 sleep 2
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker version
 sleep 2
-echo "INSTALL DOCKER COMPOSE"
+tput setaf 4; echo "INSTALL DOCKER COMPOSE"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose version
 sleep 2
-echo "GENERATING SSH KEYS"
+tput setaf 4; echo "GENERATING SSH KEYS"
 ssh-keygen
-echo "SSH KEY GENERATED SUCCESSFULLY"
+tput setaf 4;echo "SSH KEY GENERATED SUCCESSFULLY"
 sleep 2
-echo "SETUP GIT"
+tput setaf 4; echo "SETUP GIT"
 sleep 1
-echo "ENTER GIT NAME"
+tput setaf 4; echo "ENTER GIT NAME"
 read NAME
-echo $NAME
+tput setaf 4; echo $NAME
 git config --global user.name "$NAME"
 sleep 1
-echo "ENTER GIT E-MAIL"
+tput setaf 4; echo "ENTER GIT E-MAIL"
 read EMAIL
 git config --global user.email $EMAIL
 sleep 2
-echo "YOUR GIT SETTINGS"
+tput setaf 4; echo "YOUR GIT SETTINGS"
 git config --global user.name
 git config --global user.email
 sleep 2
-echo "SETUP SUCCESSFUL"
+tput setaf 10; echo "SETUP SUCCESSFUL"
