abd5c7d7fa3a931392dac70eaaba46de4d38ef68
simatei <robertkimutai7@gmail.com>
2021-08-12 12:46:17 +0300

    rewrite shell not to ignore $

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -18,16 +18,19 @@ server {
 
         location / {
         proxy_pass http://localhost:8080;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
 }
 
 EOF
-service nginx restart
+echo -e "\e[32mTesting Config File\e[0m"
+nginx -t
 
+echo -e "\e[32mRestarting Nginx\e[0m"
+service nginx restart
 
 echo -e "\e[1;32mDone!\e[0m"
