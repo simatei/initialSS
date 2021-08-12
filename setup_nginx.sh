b9628c6273e7b6af994335d35dcc6c9fbf0b3497
simatei <robertkimutai7@gmail.com>
2021-08-12 11:05:54 +0300

    add setup_nginx.sh script

---
+++ setup_nginx.sh
@@ -0,0 +1,32 @@
+
+if [ ! -f /usr/sbin/nginx ]; then
+    echo "Installing nginx"
+    apt-get install -y nginx
+fi
+
+cat > etc/nginx/sites-enabled/default <<EOF
+server {
+
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm;
+
+        server_name _;
+
+        location / {
+                proxy_pass http://localhost:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+}
+EOF
+service nginx restart
+
+
+echo -e "\e[1;32mDone!\e[0m"
